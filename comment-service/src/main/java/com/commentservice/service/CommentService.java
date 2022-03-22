1e2d497f0a4bb8c8d7a87981527affd195e83c01
Naresh-cha <94688086+Naresh-cha@users.noreply.github.com>
2022-03-22 15:41:48 +0530

    Delete comment-service directory

--- comment-service/src/main/java/com/commentservice/service/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.commentservice.service;
-
-import com.commentservice.model.Comment;
-
-public interface CommentService   {
-    Comment createPost(Comment comment);
-   // Comment deletecomment(Comment comment);
-}
-
